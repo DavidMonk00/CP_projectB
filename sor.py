6c9d30c7bfbae1d3ab94451b6acc669001339e0f
David Monk <d.g.monk0@gmail.com>
2016-11-19 18:35:19 +0000

    Updated git repository.

--- sor.py
+++ sor.py
@@ -1,13 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-x = np.zeros(81).reshape(9,9)
-for i in range(9):
-    x[i,i] = -4
-for i in range(8):
-    x[i,i+1] = 1
-    x[i+1,i] = 1
-for i in range(6):
-    x[i,i+3] = 1
-    x[i+3,i] = 1
+nx,ny = 10,10
+N = nx*ny
+A = np.zeros(N**2).reshape(N,N)
+for i in range(N):
+    A[i,i] = -4
+for i in range(N-1):
+    A[i,i+1] = 1
+    A[i+1,i] = 1
+for i in range(N-ny):
+    A[i,i+ny] = 1
+    A[i+ny,i] = 1
 
-print x
+x = np.zeros(N).reshape(N,1)
+b = np.zeros(N).reshape(N,1)
+b[0] = -1
+b[N-1] = 1
+
+print A
+
+for i in range(100):
+    x = np.dot(np.identity(N)/4,np.dot(A + 4*np.identity(N),x)-b)
+
+x = x.reshape(N).reshape(nx,ny)
+
+plt.imshow(x,interpolation='nearest',cmap='hot')
+plt.show()
